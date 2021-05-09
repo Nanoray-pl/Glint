25e26046c5afffba4bfc2cd0d555dd449fc917bb
Michał Dolaś <me@shockah.pl>
2021-05-09 02:19:27 +0200

    Handling YAML and Properties config

--- core/src/main/kotlin/pl/nanoray/glint/ConfigManager.kt
+++ core/src/main/kotlin/pl/nanoray/glint/ConfigManager.kt
@@ -1,9 +1,13 @@
 package pl.nanoray.glint
 
+import com.charleskorn.kaml.Yaml
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.properties.Properties
 import kotlinx.serialization.serializer
+import java.io.BufferedInputStream
 import java.nio.file.Path
 import kotlin.io.path.exists
+import kotlin.io.path.inputStream
 import kotlin.io.path.isRegularFile
 import kotlin.io.path.readText
 import kotlin.reflect.KClass
@@ -33,10 +37,11 @@ class ConfigManagerImpl(
 		prettyPrint = true
 		prettyPrintIndent = "\t"
 	}
+	private val propertiesFormat = Properties
+	private val yamlFormat = Yaml.default
 
 	override fun <T: Any> getConfig(type: KType, klass: KClass<T>, name: String): T? {
-//		val extensions = listOf("json", "yml", "yaml", "toml", "props", "properties")
-		return getJsonConfig(type, name)
+		return getJsonConfig(type, name) ?: getYamlConfig(type, name) ?: getPropertiesConfig(type, name)
 	}
 
 	private fun <T: Any> getJsonConfig(type: KType, name: String): T? {
@@ -47,5 +52,34 @@ class ConfigManagerImpl(
 		val serializer = serializer(type)
 		@Suppress("UNCHECKED_CAST")
 		return jsonFormat.decodeFromString(serializer, stringContent)?.let { it as T }
+	}
+
+	private fun <T: Any> getYamlConfig(type: KType, name: String): T? {
+		for (extension in listOf("yml", "yaml")) {
+			val path = configPath.resolve("${name}.$extension")
+			if (!path.exists() || !path.isRegularFile())
+				return null
+			val stringContent = path.readText()
+			val serializer = serializer(type)
+			@Suppress("UNCHECKED_CAST")
+			yamlFormat.decodeFromString(serializer, stringContent)?.let { return it as T }
+		}
+		return null
+	}
+
+	private fun <T: Any> getPropertiesConfig(type: KType, name: String): T? {
+		for (extension in listOf("props", "properties")) {
+			val path = configPath.resolve("${name}.$extension")
+			if (!path.exists() || !path.isRegularFile())
+				return null
+			val stringContent = path.readText()
+			val properties = java.util.Properties().apply { BufferedInputStream(path.inputStream()).use { load(it) } }
+			val serializer = serializer(type)
+			@Suppress("UNCHECKED_CAST")
+			propertiesFormat.decodeFromMap(serializer, properties.toMap() as Map<String, Any>)
+			@Suppress("UNCHECKED_CAST")
+			yamlFormat.decodeFromString(serializer, stringContent)?.let { return it as T }
+		}
+		return null
 	}
 }
